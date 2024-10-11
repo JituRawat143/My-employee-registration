617c8eb5c4adea4dbe2a2cd892502a84b76e9679
JituRawat143 <jitendrarawat7451@gmail.com>
2024-10-11 12:19:38 +0530

    Updated components after modification

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import EmployeeData from './loginAndRegistration/employeedata';
 import LoginPage from './Loginpage/LoginPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
+import './Style/style.css'
 const App = () => {
   return (
     
--- src/Loginpage/LoginPage.jsx
+++ src/Loginpage/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import img1 from '../Pics/p-m-2.png';
-
+ 
 
 function LoginPage() {  
   const superadmin = {
